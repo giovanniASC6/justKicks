b6e2d0dd987b5cd1c32a996c7197dca920bf935a
giovanniASC6 <giovanni.owens@gmail.com>
2019-08-13 15:00:36 -0400

    donation-page

---
+++ donationPage/script.js
@@ -0,0 +1,54 @@
+let firstName = "";
+		let lastName = "";
+		let email = "";
+		let dType = "";
+		let receipt = "";
+		let anon = "";
+		let list = "";
+		let amount = "";
+		
+		$('.set-amount').autoGrow(0);
+		
+		/*
+			if(isiPad || jQuery.browser.mobile){
+				$('#team').hide()
+				$('.team-mobile').show();	
+			}else{
+				$('#team').show()
+				$('.team-mobile').hide();
+			}
+		*/
+		
+		//Set & Highlight Donation Amount
+		$(".button").click(function(){
+			$(".button").removeClass("selected");
+			$(this).addClass("selected");
+			
+			$(this).find("input").focus();
+		});
+		
+		//Grow the donation box if they type more than 4 numbers
+		$(".set-amount").keyup(function(){
+			
+			if (this.value != this.value.replace(/[^0-9\.]/g, '')) {
+		       this.value = this.value.replace(/[^0-9\.]/g, '');
+		    }
+
+		});
+		
+		
+		$("input").on("change", function(){
+			// $(".donation-box").css("height", "500px");
+			
+			firstName = $("#firstName").val();
+			lastName = $("#lastName").val();
+			email = $("#email").val();
+			
+			if ( $("#one-time").prop( "checked" ) ){
+				dType = "One-Time";
+			}
+			if ( $("#monthly").prop( "checked" ) ){
+				dType = "Monthly";
+			}
+
+		});
